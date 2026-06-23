cd49c34b6e9f76e8eb3d2d7a088c695ea0907d1e
Fabrício FJ <haksubja@gmail.com>
2026-06-23 07:02:51 -0300

    draw circle example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from manimdrone!")
+from manim import *
 
 
-if __name__ == "__main__":
-    main()
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
